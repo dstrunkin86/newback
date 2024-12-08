f708efe0755dd3e43627e147d7e54e5e239377fb
Dmitry Strunkin <dstrunkin@gmail.com>
2024-12-08 20:32:53 +0300

    fix for personal_access_tokens

--- app/Console/Commands/MigrateOldArthallUsers.php
+++ app/Console/Commands/MigrateOldArthallUsers.php
@@ -32,7 +32,7 @@ class MigrateOldArthallUsers extends Command
     {
         $num_tokens = OldArthallPersonalAccessToken::where('last_used_at', '>', '2023-01-01')->count();
         $this->line($num_tokens  . ' users to be created');
-        $tokens = OldArthallPersonalAccessToken::where('last_used_at', '>', '2023-01-01')->chunk(10000, function ($tokens) {
+        $tokens = OldArthallPersonalAccessToken::where('last_used_at', '>', '2023-01-01')->chunkByIdDesc(10000, function ($tokens) {
             $token = null;
             foreach ($tokens as $token) {
                 //DB::connection('mysql_old_arthall')->enableQueryLog();
